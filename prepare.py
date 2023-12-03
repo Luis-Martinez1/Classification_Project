57e348170b1329b25dbd0f2d8f3d5d00670371c4
Luis Martinez <luis.martinez.sat@gmail.com>
2023-12-02 21:46:10 -0600

    changed functions

--- prepare.py
+++ prepare.py
@@ -4,23 +4,29 @@ from sklearn.model_selection import train_test_split
 
 
 
-
-
-
-
 def prep_telco(df):
     '''Takes in the telco churn dataframe as an argument and returns the dataframe
     with unnecessary columns dropped and empty values in the [total_charges] column
     changed from a blank space to a zero.
     '''
-    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id'], inplace=True)
+    columns_to_sum = ['online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies']
+    # New column that looks through each row and when a "yes" is found in columns_to_sum, it adds a 1
+    df['total_add_on_count'] = df[columns_to_sum].apply(lambda row: sum(1 if value == 'Yes' else 0 if value == 'No' else 9 for value in row), axis=1)
+    # change the number 54 to no internet service
+    df['total_add_on_count'].replace({54:'No internet service'}, inplace=True)
+
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'phone_service'], inplace=True)
     df.total_charges = df.total_charges.str.replace(' ', '0.0')
     df.internet_service_type.fillna('No internet service', inplace = True)
     df['total_charges'] = df['total_charges'].astype(float)
     df['senior_citizen'].replace({0:'No', 1: 'Yes'}, inplace=True)
+    df.rename(columns={'multiple_lines': 'phone_service_type'}, inplace=True)
+    df['phone_service_type'].replace({'Yes':'Multiple lines', 'No': 'Single line'}, inplace=True)
     df = df.set_index('customer_id')
+    
 
     return df
+
 
 
 
@@ -30,8 +36,8 @@ def prep_telco(df):
 def split_data(df, target):
     """
     This function takes in any DataFrame and a target variable as an argument 
-    and returns train, validate, and test variables stratifying on the target variable.
-    It returns these variables as DataFrames with a printout of their proportion to the original DataFrame.
+    and returns train, validate, and test dataframes stratifying on the target variable.
+    It returns three DataFrames with a printout of their proportion to the original DataFrame.
     """
     train, validate_test = train_test_split(
         df, train_size=0.6, random_state=123, stratify=df[target]
@@ -50,30 +56,56 @@ def split_data(df, target):
 
 
 
+
+
+
+
+
 def preprocess_telco(train_df, val_df, test_df):
     '''
-    preprocess_telco will take in three pandas dataframes
-    of an acquired and prepared telco data set that has also been split into train, validate, and test.
-    
-    output:
-    encoded, ML-ready versions of train, validate, and test with 
-    object type colummncs encoded in the one-hot fashion
-    return: (pd.DataFrame, pd.DataFrame, pd.DataFrame)
+    This function takes three DataFrames as an argument 
+    and returns train, validate, and test dataframes ready for machine learning.
+    It returns DataFrames with all string values changed to represent thier binary equivalent.
     '''
-     
-    encoding_vars = []
-    # loop through the columns to fill encoded_vars with appropriate datatype 
-    for col in train_df.columns:
-        if train_df[col].dtype == 'O':
-            encoding_vars.append(col)
-    # initialize an empty list to hold our encoded dataframes:
+    
+    columns_to_convert = ['gender', 'senior_citizen', 'partner', 'dependents', 'online_security', 'online_backup', 'device_protection',
+                          'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'churn']
+
+    encoding_vars = ['contract_type', 'internet_service_type', 'payment_type', 'phone_service_type']
+
     encoded_dfs = []
     for df in [train_df, val_df, test_df]:
-        df_encoded_cats = pd.get_dummies(
-            df[encoding_vars],
-              drop_first=True).astype(int)
-        encoded_dfs.append(pd.concat(
-            [df,
-            df_encoded_cats],
-            axis=1).drop(columns=encoding_vars))
+        df_encoded_cats = pd.get_dummies(df[encoding_vars], drop_first=True).astype(int)
+        df = pd.concat([df, df_encoded_cats], axis=1).drop(columns=encoding_vars)
+        
+        # Replace certain string values with numerical values
+        df.replace({"No": 0, "Yes": 1, "Male": 1, "Female": 0, "No internet service": 0}, inplace=True)
+        
+        # Convert specified columns to integer type
+        df[columns_to_convert] = df[columns_to_convert].astype(int)
+        
+        encoded_dfs.append(df)
+
     return encoded_dfs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
